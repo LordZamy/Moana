7f37522cdbd218765dfe1242bfc6a95d7aba026b
reecedantin <reecedantin@yahoo.com>
2017-02-21 21:33:35 -0500

    Added ProfileActvity to manifest and stopped error on profile edit

--- app/src/main/java/moanainc/com/moana/controllers/ProfileActivity.java
+++ app/src/main/java/moanainc/com/moana/controllers/ProfileActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 import moanainc.com.moana.R;
 
@@ -16,11 +17,16 @@ public class ProfileActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.register_activity);
+        setContentView(R.layout.profile_activity);
     }
 
     public void goToApplication(View view) {
         Intent goToApplication = new Intent(getBaseContext(), ScreenA.class);
         getBaseContext().startActivity(goToApplication);
+    }
+
+    public void onSavePressed(View view){
+        Toast toast = Toast.makeText(getApplicationContext(), "Failed: Not implemented", Toast.LENGTH_LONG);
+        toast.show();
     }
 }
