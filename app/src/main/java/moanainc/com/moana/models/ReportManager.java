aa0784690c98179542e4da10a2cbc09cb48bd5d1
jbaldwin33 <jbaldwin93@gatech.edu>
2017-03-16 18:22:24 -0400

    added choose report activity and screen. added to manifest

--- app/src/main/java/moanainc/com/moana/models/ReportManager.java
+++ app/src/main/java/moanainc/com/moana/models/ReportManager.java
@@ -1,12 +1,16 @@
 package moanainc.com.moana.models;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by darrion on 2/28/17.
  */
 
 public class ReportManager {
+
+    public static List<String> legalReports = Arrays.asList("Availability", "Purity", "History", "Source");
 
     ArrayList<Report> _reports = new ArrayList<Report>();
 
