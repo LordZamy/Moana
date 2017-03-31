e62bc563193ea4ea56fb5f22059d3b3f34eea317
gitdarrion <dbanks35@gatech.edu>
2017-03-31 17:04:15 -0400

    Moana fullscreen.

--- app/src/main/java/moanainc/com/moana/controller/HomeActivity.java
+++ app/src/main/java/moanainc/com/moana/controller/HomeActivity.java
@@ -1,9 +1,14 @@
 package moanainc.com.moana.controller;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import moanainc.com.moana.R;
@@ -14,10 +19,22 @@ import moanainc.com.moana.R;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private Button mLoginButton;
+    private Button mRegisterButton;
+    private TextView mTitleView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_activity);
+
+        mLoginButton = (Button) findViewById(R.id.main_goto_login_button);
+        mRegisterButton = (Button) findViewById(R.id.main_goto_register_button);
+        mTitleView = (TextView) findViewById(R.id.titleView);
+        Typeface disney_font = Typeface.createFromAsset(getAssets(),  "fonts/disneyui.ttf");
+        mLoginButton.setTypeface(disney_font);
+        mRegisterButton.setTypeface(disney_font);
+        mTitleView.setTypeface(disney_font);
     }
 
     public void gotoLogin(View view) {
