b4425ece00ce0e456c49e5d0c2be125746d079b4
jbaldwin33 <jbaldwin93@gatech.edu>
2017-04-07 20:43:35 -0400

    edited junit test

--- app/src/test/java/moanainc/com/moana/AddPurityReportTests.java
+++ app/src/test/java/moanainc/com/moana/AddPurityReportTests.java
@@ -4,18 +4,21 @@ import org.junit.Before;
 import org.junit.Test;
 
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Date;
 
 import moanainc.com.moana.model.Report;
 import moanainc.com.moana.model.report.PurityCondition;
 import moanainc.com.moana.model.report.PurityReport;
+import moanainc.com.moana.model.user.AccountType;
 import moanainc.com.moana.model.user.User;
+import moanainc.com.moana.model.user.Worker;
 
 import static junit.framework.Assert.assertEquals;
 
 /**
- * Created by USER on 4/5/2017.
+ * Created by josh baldwin on 4/5/2017.
  */
 
 public class AddPurityReportTests {
@@ -30,25 +33,37 @@ public class AddPurityReportTests {
         User creator = new User("dede", "dede");
         PurityCondition condition = PurityCondition.SAFE;
         goodPurityReport = new PurityReport("name", date, creator, 12, 12, condition, 12, 12);
-
-        //badPurityReport = new PurityReport(null, null, null, 0, 0, null, 0, 0);
         purityReports.add(goodPurityReport);
     }
 
     @Test
-    public void testAddNullReport() {
+    public void testAddEmptyStringReport() {
         try {
-            badPurityReport = new PurityReport(null, null, null, 0, 0, null, 0, 0);
-        } catch (NullPointerException e) {
-            System.err.println("NullPointerException: " + e.getMessage());
+            badPurityReport = new PurityReport("", null, null, 0, 0, null, Integer.parseInt(""), Integer.parseInt(""));
+            purityReports.add(badPurityReport);
+        } catch (NumberFormatException e) {
+            System.err.println("NumberFormatException: Cannot parse an empty string.");
         }
-        purityReports.add(badPurityReport);
-        assertEquals(2, purityReports.size());
+
+        assertEquals(1, purityReports.size());
 
     }
 
     @Test
     public void testAddPurityReport() {
         assertEquals(1, purityReports.size());
+    }
+
+    @Test
+    public void testAddEmptyFieldReport() {
+        Date date = new Date(2014444);
+        User creator = new User("Weeeee", "dede");
+        PurityCondition condition = PurityCondition.UNSAFE;
+        Report newReport = new PurityReport("", date, creator, 12, 12, condition, 12, 12);
+        try {
+            newReport.getName().charAt(1);
+        } catch (StringIndexOutOfBoundsException e) {
+            System.err.println("Cannot leave name field blank.");
+        }
     }
 }
