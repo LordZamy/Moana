8a4eafac46623041fcbd5bd0b58bfee9189e68d7
gitdarrion <dbanks35@gatech.edu>
2017-04-11 18:40:14 -0400

    More issues cleared.

--- app/src/main/java/moanainc/com/moana/model/user/Account.java
+++ app/src/main/java/moanainc/com/moana/model/user/Account.java
@@ -4,9 +4,7 @@ package moanainc.com.moana.model.user;
  * Created by josh baldwin on 2/13/2017.
  */
 
-public class Account {
-    // the user's permission level
-    private String _permission;
+class Account {
     //the user's username
     private String _username;
     //the user's password
@@ -20,7 +18,7 @@ public class Account {
     private String _emailAddress = "";
     private String _homeAddress = "";
 
-    public Account() {
+    Account() {
 
     }
 
@@ -32,7 +30,7 @@ public class Account {
      * @param accountType the account type
      */
 
-    public Account(String username, String password, String name, AccountType accountType) {
+    Account(String username, String password, String name, AccountType accountType) {
 
         _username = username;
         _password = password;
@@ -46,7 +44,7 @@ public class Account {
      * @param password the password
      */
 
-    public Account(String username, String password) {
+    Account(String username, String password) {
 
         _username = username;
         _password = password;
@@ -56,7 +54,6 @@ public class Account {
      * Getters and Setters
      */
     public String getUsername() { return _username; }
-    public void setUsername(String username) { _username = username; }
 
     public String getPassword() { return _password; } //if we want to implement "Forgot password"
     public void setPassword(String password) { _password = password; } //if we want to implement "Change password"
@@ -65,21 +62,12 @@ public class Account {
     public void setName(String name) { _name = name; }
 
     public AccountType getAccountType() { return _accountType; }
-    public void setAccountType(AccountType type) { _accountType = type; }
 
     public String getEmailAddress() { return _emailAddress; }
     public void setEmailAddress(String email) { _emailAddress = email; }
 
     public String getHomeAddress() { return _homeAddress; }
     public void setHomeAddress(String home) { _homeAddress = home; }
-
-    public String get_permission() {
-        return _permission;
-    }
-
-    public void set_permission(String _permission) {
-        this._permission = _permission;
-    }
 
     @Override
     public boolean equals(Object o) {
