f03becc58da3eee58fe073c39657ebac6507c193
Samarth Wahal <samarthwahal@hotmail.com>
2017-04-12 01:22:52 -0400

    Updated to test with random data

--- app/src/test/java/moanainc/com/moana/PopulateGraphPointsTests.java
+++ app/src/test/java/moanainc/com/moana/PopulateGraphPointsTests.java
@@ -1,14 +1,21 @@
 package moanainc.com.moana;
 
+import android.util.Log;
+
 import com.jjoe64.graphview.series.DataPoint;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Random;
 
 import moanainc.com.moana.controller.HistoricalReportActivity;
 import moanainc.com.moana.model.Report;
+import moanainc.com.moana.model.report.PurityCondition;
+import moanainc.com.moana.model.report.PurityReport;
+import moanainc.com.moana.model.user.User;
 
 import org.junit.Assert;
 
@@ -19,6 +26,7 @@ import org.junit.Assert;
 public class PopulateGraphPointsTests {
     private ArrayList<Report> emptyReportList;
     private ArrayList<Report> nullReportList;
+    private ArrayList<Report> randomReportList;
 
     @Before
     public void setup() {
@@ -48,6 +56,32 @@ public class PopulateGraphPointsTests {
         DataPoint[] actual = HistoricalReportActivity.populateGraphPoints(nullReportList);
 
         checkDataPointArraysEqual(expected, actual);
+    }
+
+    @Test
+    public void testPurityReportListRandom() {
+        Random rand = new Random();
+
+        // perform 100 iterations
+        for (int testNum = 0; testNum < 100; testNum++) {
+            randomReportList = new ArrayList<>(0);
+            Report randomReport;
+            for (int i = 0; i < 100; i++) {
+                randomReport = new PurityReport("random", new Date(Math.abs(rand.nextLong())), new User(),
+                        0.0, 0.0, PurityCondition.SAFE, rand.nextInt(500), 0);
+                randomReportList.add(randomReport);
+            }
+
+            DataPoint[] actual = HistoricalReportActivity.populateGraphPoints(randomReportList);
+
+            for (int i = 0; i < actual.length; i++) {
+                Assert.assertTrue(actual[i].getY() - 0.001 <= 500);
+                Assert.assertTrue(actual[i].getY() + 0.001 >= 0);
+
+                Assert.assertTrue(actual[i].getX() <= 12);
+                Assert.assertTrue(actual[i].getX() >= 1);
+            }
+        }
     }
 
     private void checkDataPointArraysEqual(DataPoint[] expected, DataPoint[] actual) {
